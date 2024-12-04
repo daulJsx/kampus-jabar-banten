c38eadc74fade039361776757f38bbd96bd411ef
daulJsx <mrdaul09@gmail.com>
2024-12-04 10:14:01 +0700

    feat: can render come component using screen

--- tests/rookie.nuxt.spec.ts
+++ tests/rookie.nuxt.spec.ts
@@ -1,16 +1,22 @@
 import { it, expect } from "vitest";
 import type { Component } from "vue";
-declare module "#components" {
-  export const CompToTest: Component;
-}
-// ---cut---
-// tests/components/SomeComponents.nuxt.spec.ts
+import { renderSuspended } from "@nuxt/test-utils/runtime";
+import { screen } from "@testing-library/vue";
 import { mountSuspended } from "@nuxt/test-utils/runtime";
 import CompToTest from "~/components/CompToTest.vue";
+
+declare module "#components" {
+  export const ComponentToTest: Component;
+}
 
 it("can mount some component", async () => {
   const component = await mountSuspended(CompToTest);
   expect(component.text()).toMatchInlineSnapshot(
     '"This is an auto-imported component"'
   );
+});
+
+it("can render some component", async () => {
+  await renderSuspended(CompToTest);
+  expect(screen.getByText("This is an auto-imported component")).toBeDefined();
 });
